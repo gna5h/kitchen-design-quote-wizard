1e2562e5f7ac2d2037a9e41c4f83d188caff7e96
gna5h <gg.orlanes@gmail.com>
2024-12-05 10:01:52 +0800

    feat: add wizard routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\KitchenWizardController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/wizard/step1', [KitchenWizardController::class,'showStep1'])->name('wizard.step1');
+Route::post('/wizard/step1', [KitchenWizardController::class,'processStep1']);
+Route::get('/wizard/step2', [KitchenWizardController::class,'showStep2'])->name('wizard.step2');
+Route::post('/wizard/step2', [KitchenWizardController::class,'processStep2']);
 
 Route::get('/', function () {
     return view('welcome');
