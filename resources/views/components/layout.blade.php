5ef7ff4655656e42463102748f9f56d4999cd855
gna5h <gg.orlanes@gmail.com>
2024-12-05 13:13:07 +0800

    feat: add step 1 display and process integration

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kitchen Design Quote Wizard</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="m-5">
+        {{ $slot }}
+    </body>
+</html>
+
