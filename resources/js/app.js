b73c3929d7fa8c1765cfc4367830d01012848812
gna5h <gg.orlanes@gmail.com>
2024-12-05 15:25:27 +0800

    feat: add success toaster

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toast = document.getElementById("toast-simple");
+    if (toast) {
+        setTimeout(() => {
+            toast.classList.add("hide");
+            setTimeout(() => {
+                toast.style.display = "none";
+            }, 500);
+        }, 3000);
+    }
+});
