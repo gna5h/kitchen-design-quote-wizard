04119263cf19c40336af269df02002aaac4387b5
gna5h <gg.orlanes@gmail.com>
2024-12-06 19:58:02 +0800

    fix: update redirect home route

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\KitchenWizardController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/wizard/step1')->name('home');
+
 Route::get('/wizard/step1', [KitchenWizardController::class,'showStep1'])->name('wizard.step1');
 Route::post('/wizard/step1', [KitchenWizardController::class,'processStep1']);
 Route::get('/wizard/step2', [KitchenWizardController::class, 'showStep2'])->name('wizard.step2');
 Route::post('/wizard/step2', [KitchenWizardController::class, 'processStep2']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
